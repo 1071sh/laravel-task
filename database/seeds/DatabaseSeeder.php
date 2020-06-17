398c80de84b235112276b4bc2e18083d7a36517a
nishida <k.nishida@vistage-llc.com>
2020-06-17 13:11:15 +0900

    section3

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // DatabaseSeeder.phpで使用するSeederファイルを指定する
+        $this->call(AgesTableSeeder::class);
     }
 }
